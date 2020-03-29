08650857f4d928ba4613e9d7bfc46673004f02a9
RaymondLoranger <Raymond.Loranger@bell.net>
2020-03-29 19:57:34 -0400

    0.1.13

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Islands.Client.Summary.MixProject do
   def project do
     [
       app: :islands_client_summary,
-      version: "0.1.12",
+      version: "0.1.13",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       name: "Islands Client Summary",
